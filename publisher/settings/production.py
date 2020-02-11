a7c94153075de57b7fec7ecf8d51eea3589cf237
kkWeGo <nicolapasqua99@gmail.com>
2020-02-11 18:50:42 +0100

    Settings up heroku

--- publisher/settings/production.py
+++ publisher/settings/production.py
@@ -6,12 +6,6 @@ ENVIRONMENT = 'production'
 DEBUG = True
 ALLOWED_HOSTS = ['createbeautifuldocs.herokuapp.com']
 DATABASES['default'] = dj_database_url.config(
-    default='postgres://wdtktiblvnhkzz:8e5a9fd91df352a0718334c3a5f33eb59eeb30dad1bfa5e0c17b3080ec9a40d0@ec2-184-72-235-80.compute-1.amazonaws.com:5432/d8mshascf4h594'
+    default='postgres://sfaaadipuwcvoi:98c7af63806a5409eb0dea2e9eb8add4430a283e7d11f0a72697028fa1e2042c@ec2-46-137-156-205.eu-west-1.compute.amazonaws.com:5432/dc6998hfm9pefm'
 )
-#DATABASES = {
- #   'default': 
- #       dj_database_url.config(
- #           'postgres://rzbuurxglahzlw:a8f18093a4f3d1bdd25498981c69fc5e9ef6bd0a829d2a40383ea65a7affddda@ec2-34-197-171-33.compute-1.amazonaws.com:5432/dl9ab8mrb9kbo'
-  #      )
-#}
 django.setup()
